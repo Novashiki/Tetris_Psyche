ab8f660764ae93d9b360e65ee6f080168082c37f
Aabir Dutta <aabir.dutta.04@gmail.com>
2025-12-06 14:09:27 +0530

    Initial commit

---
+++ Tetris/main.py
@@ -0,0 +1,36 @@
+import pygame, sys
+from grid import Grid
+from blocks import *
+
+pygame.init()
+dark_blue = (44, 44, 127)
+
+screen = pygame.display.set_mode((300, 600))
+pygame.display.set_caption("Tetris Pysche")
+
+clock = pygame.time.Clock()
+
+game_grid = Grid()
+
+block = OBlock()
+
+# game_grid.grid[0][0] = 1
+# game_grid.grid[3][5] = 4
+# game_grid.grid[17][8] = 7
+# game_grid.grid[15][4] = 3
+
+game_grid.print_grid()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill(dark_blue)
+    game_grid.draw(screen)
+    block.draw(screen)
+
+    pygame.display.update()
+    clock.tick(60)
